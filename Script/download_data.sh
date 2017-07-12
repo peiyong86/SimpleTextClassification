6f6c9cbf1e211e2b209d08585ec033f8d86d8997
Yong Pei <yong.pei@sap.com>
2017-07-12 14:24:46 +0800

    add extract code to download script

--- Script/download_data.sh
+++ Script/download_data.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-
+mkdir ./Data
+mkdir ./Data/raw
+mkdir ./Data/extract
 wget -P ./Data/raw http://www.cs.cornell.edu/people/pabo/movie-review-data/rt-polaritydata.tar.gz
+tar zxvf ./Data/raw/rt-polaritydata.tar.gz -C ./Data/extract
